db16fec0245812274d57fc7d670a69aa55e74c96
Augustine2630 <maksim2630@gmail.com>
2024-01-05 18:51:17 +0300

    lab 2

---
+++ SecondPage.tsx
@@ -0,0 +1,81 @@
+import {
+  Alert,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import Spacer from './Spacer.tsx';
+
+export function SecondPage() {
+  const [number, onChangeNumber] = React.useState('');
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  var buttonProps = {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: isEnabled ? 'red' : 'blue',
+  };
+  return (
+    <SafeAreaView>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="Enter the value"
+          keyboardType="numeric"
+        />
+        <Pressable
+          {...buttonProps}
+          onPress={() => Alert.alert('Your value is ' + number)}>
+          <Text style={styles.text}>{'Know value'}</Text>
+        </Pressable>
+      </SafeAreaView>
+      <Spacer size={10} />
+      <SafeAreaView style={styles.container}>
+        <Switch
+          trackColor={{false: '#767577', true: '#81b0ff'}}
+          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={() => {
+            toggleSwitch();
+          }}
+          value={isEnabled}
+        />
+        <Text style={{fontSize: 26, fontWeight: 'bold', letterSpacing: 0.25}}>
+          {'Change button color'}
+        </Text>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  container: {
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    gap: 10,
+  },
+});
